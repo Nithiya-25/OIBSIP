950abb661e6c1d4a607ae8f00159f80d2ada89c7
Nithiya-25 <25ad105@rmkcet.ac.in>
2026-06-08 12:19:53 +0530

    Update app.py

--- OneDrive/Documents/Desktop/OIBSIP/app.py
+++ OneDrive/Documents/Desktop/OIBSIP/app.py
@@ -1,20 +1,21 @@
+import os
 import streamlit as st
 import pandas as pd
 import joblib
 
-# Load trained model
-model = joblib.load("model.pkl")
+# absolute safe path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "model.pkl")
+
+model = joblib.load(model_path)
 
 st.title("📈 Sales Prediction App")
 
 tv = st.number_input("TV Advertising Budget", min_value=0.0)
-
 radio = st.number_input("Radio Advertising Budget", min_value=0.0)
-
 newspaper = st.number_input("Newspaper Advertising Budget", min_value=0.0)
 
 if st.button("Predict Sales"):
-
     user_input = pd.DataFrame(
         [[tv, radio, newspaper]],
         columns=["TV", "Radio", "Newspaper"]
@@ -22,6 +23,4 @@ if st.button("Predict Sales"):
 
     prediction = model.predict(user_input)
 
-    st.success(
-        f"Predicted Sales: {prediction[0]:.2f}"
-    )
+    st.success(f"Predicted Sales: {prediction[0]:.2f}")
